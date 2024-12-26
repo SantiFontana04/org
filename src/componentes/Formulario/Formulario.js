48b8f91c57db7c08684879b1dc59653d31cde1f1
SantiFontana <santiagofontana61@gmail.com>
2024-12-26 19:51:45 -0300

    Agregando el boton al formulario el cual ahora guarda los valores puestos en los input y agregando la sección MiOrg con su imagen add.png

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,13 +1,59 @@
+import { use, useState } from "react"
 import "./Formulario.css"
 import CampoTexto from "../Campo Texto"
+import ListaOpciones from "../ListaOpciones"
+import Boton from "../Boton"
 
 const Formulario = () => {
+
+    const [nombre, actualizarNombre] = useState("")
+    const [puesto, actualizarPuesto] = useState("")
+    const [foto, actualizarFoto] = useState("")
+    const [equipo, actualizarEquipo] = useState("")
+
+    const manejarEnvio = (e) => {
+        e.preventDefault()
+        console.log("Manejar el envio")
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto,
+            equipo
+        }
+        console.log(datosAEnviar)
+    }
+
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" />
-            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
-            <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
+            <CampoTexto 
+                titulo="Nombre" 
+                placeholder="Ingresar nombre" 
+                required 
+                valor={nombre} 
+                actualizarValor={actualizarNombre} 
+            />
+            <CampoTexto 
+                titulo="Puesto" 
+                placeholder="Ingresar puesto" 
+                required
+                valor={puesto} 
+                actualizarValor={actualizarPuesto} 
+            />
+            <CampoTexto 
+                titulo="Foto" 
+                placeholder="Ingresar enlace de foto" 
+                required 
+                valor={foto} 
+                actualizarValor={actualizarFoto} 
+            />
+            <ListaOpciones 
+                valor={equipo}
+                actualizarEquipo={actualizarEquipo}
+            />
+            <Boton>
+                Crear
+            </Boton>
         </form>
     </section>
 }
