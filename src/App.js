658bdb99ccee8a2a85dc30df73d7773661e8a202
SantiFontana <santiagofontana61@gmail.com>
2024-12-28 18:01:14 -0300

    Agregando ids con uuid, creando un segindo formulario con la funcionalidad para crear nuevos equipos y agregando iconos de redes para el footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuid } from "uuid"
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
@@ -9,30 +10,35 @@ import Footer from './componentes/Footer';
 function App() {
     const [mostrarFormulario, actualizarMostrar] = useState(false)
     const [colaboradores, actualizarColaboradores] = useState([{
+        id: uuid(),
         equipo: "Front End",
         foto: "https://github.com/SantiFontana04.png",
         nombre: "Santiago Fontana",
         puesto: "Estudiante"
     },
     {
+        id: uuid(),
         equipo: "Front End",
         foto: "https://github.com/harlandlohora.png",
         nombre: "Harland Lohora",
         puesto: "Instructor"
     },
     {
+        id: uuid(),
         equipo: "UX y Diseño",
         foto: "https://github.com/JeanmarieAluraLatam.png",
         nombre: "Jeanmarie Quijada",
         puesto: "Instructora en Alura Latam"
     },
     {
+        id: uuid(),
         equipo: "Programación",
         foto: "https://github.com/christianpva.png",
         nombre: "Christian Velasco",
         puesto: "Head de Alura e Instructor"
     },
     {
+        id: uuid(),
         equipo: "Innovación y Gestión",
         foto: "https://github.com/JoseDarioGonzalezCha.png",
         nombre: "Jose Gonzalez",
@@ -42,36 +48,43 @@ function App() {
 const [equipos, actualizarEquipos] = useState([
 
     {
+        id: uuid(),
         titulo: "Programación",
         colorPrimario: "#57c278",
         colorSecundario: "#d9f7e9"
     },
     {
+        id: uuid(),
         titulo: "Front End",
         colorPrimario: "#82cffa",
         colorSecundario: "#E8F8FF"
     },
     {
+        id: uuid(),
         titulo: "Data Science",
         colorPrimario: "#a6d157",
         colorSecundario: "#f0f8e2"
     },
     {
+        id: uuid(),
         titulo: "Devops",
         colorPrimario: "#e06b69",
         colorSecundario: "#fde7e8"
     },
     {
+        id: uuid(),
         titulo: "UX y Diseño",
         colorPrimario: "#db6ebf",
         colorSecundario: "#fae9f5"
     },
     {
+        id: uuid(),
         titulo: "Móvil",
         colorPrimario: "#ffba05",
         colorSecundario: "#fff5d9"
     },
     {
+        id: uuid(),
         titulo: "Innovación y Gestión",
         colorPrimario: "#ff8a29",
         colorSecundario: "#ffeedf"
@@ -93,11 +106,18 @@ const registrarColaborador = (colaborador) => {
     actualizarColaboradores([...colaboradores, colaborador])
 }
 
+//Eliminar colaborador
+const eliminarColaborador = (id) => {
+    console.log("Eliminar colaborador", id)
+    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id)
+    actualizarColaboradores(nuevosColaboradores)
+}
+
 //Actualizar color de equipo
-const actualizarColor = (color, titulo) => {
-    console.log("Actualizar: ", color, titulo)
+const actualizarColor = (color, id) => {
+    console.log("Actualizar: ", color, id)
     const equiposActualizados = equipos.map((equipo) => {
-        if(equipo.titulo === titulo) {
+        if(equipo.id === id) {
             equipo.colorPrimario = color
         }
         return equipo
@@ -106,9 +126,10 @@ const actualizarColor = (color, titulo) => {
     actualizarEquipos(equiposActualizados)
 }
 
-//Eliminar colaborador
-const eliminarColaborador = () => {
-    console.log("Eliminar colaborador")
+//Crear equipo
+const crearEquipo = (nuevoEquipo) => {
+    console.log(nuevoEquipo)
+    actualizarEquipos([...equipos, { ...nuevoEquipo, id: uuid() }])
 }
 
 return (
@@ -118,6 +139,7 @@ return (
     {mostrarFormulario && <Formulario
         equipos={equipos.map( (equipo) => equipo.titulo )}
         registrarColaborador={registrarColaborador}
+        crearEquipo={crearEquipo}
     />
     }
 
--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,18 +3,11 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
     <div className='redes'>
-        <a href='https://github.com/SantiFontana04'>
-            <img src="/img/instagram.png" alt='GitHub' />
-        </a>
-        <a href='https://www.linkedin.com/in/santiago-fontana-/'>
-            <img src="/img/instagram.png" alt='Linkedin' />
-        </a>
-        <a href='https://www.instagram.com/santi.fontanaa/'>
-            <img src="/img/instagram.png" alt='Instagram' />
-        </a>
+        <a href="https://www.linkedin.com/in/santiago-fontana-/"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://github.com/SantiFontana04"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.instagram.com/santi.fontanaa/"><i class="fa-brands fa-instagram"></i></a>
     </div>
-    <img src='/img/Logo.png' alt='org' />
-    <strong>Desarrollado por Santiago Fontana</strong>
+    <strong>Santiago Fontana</strong>
 </footer>
 }
 
